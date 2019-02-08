f69340f94dc9ed952383c5c37b0bba16ec97a9c1
Karen V <karenvo@usc.edu>
2019-02-08 15:29:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
-  if array.size == 2
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
     return array.join(" and ")
-  else array.size > 2
+  else 
     return array[0..-2].join(', ') + ", and" + array[-1]
   end
 end
